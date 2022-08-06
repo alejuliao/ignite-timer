1900304d5631145db2d56f3981f1801fa0fec2c8
alejuliao <alexandrejuliao@icloud.com>
2022-08-06 19:14:40 -0300

    config extend DefaultTheme

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -10,7 +10,7 @@ export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 100px;
   height: 40px;
 
-  border-radius: 4px;
+  border-radius: 8px;
   border: 0;
   margin: 8px;
 
